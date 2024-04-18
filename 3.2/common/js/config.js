2a6a9dbee74233ac97e10107b606a065b1c5ba60
mcasanueva1 <116030641+mcasanueva1@users.noreply.github.com>
2024-04-18 14:02:13 -0300

    Added interactionSummary to config

--- 3.2/common/js/config.js
+++ 3.2/common/js/config.js
@@ -189,6 +189,75 @@ let com_idc_params = {
       "portrait": null
     }
   },
+  "interactionSummary": {
+    "active": false,
+    "fields": {
+      "Call2_vod__c": [
+        "ID",
+        "Call_Channel_vod__c",
+        "Call_Date_vod__c",
+        "Status_vod__c"
+      ],
+      "Call2_Key_Message_vod__c": [
+        "ID",
+        "Call2_vod__c", 
+        "Call_Date_vod__c", 
+        "Key_Message_vod__c", 
+        "Duration_vod__c", 
+        "Reaction_vod__c", 
+        "Display_Order_vod__c"
+      ],
+      "Sent_Email_vod__c": [    
+        "ID",
+        "Email_Fragments_vod__c",
+        "Approved_Email_Template_vod__c",
+        "Email_Sent_Date_vod__c",
+        "Opened_vod__c",
+        "Open_Count_vod__c",
+        "Last_Open_Date_vod__c",
+        "Click_Count_vod__c",
+        "Last_Activity_Date_vod__c",
+        "Approved_Document_Views_vod__c",
+        "Account_Email_vod__c",
+        "Status_vod__c",
+        "Failure_Msg_vod__c",
+      ],
+      "Email_Activity_vod__c": [
+        "ID",
+        "Sent_Email_vod__c",
+        "Approved_Document_vod__c",
+        "Vault_Doc_ID_vod__c",
+        "Vault_Doc_Name_vod__c",
+        "Vault_Document_Number_vod__c",
+        "Event_type_vod__c",
+        "Activity_DateTime_vod__c"
+      ]
+    },
+    "nonEmailCartItems": {
+      "templates": [
+        {
+          "id": null,
+          "title": null,
+          "thumb": null,
+          "vaultId": {
+            "development": null,
+            "release": null
+          },
+          "fragments": [
+            {
+              "id": null,
+              "title": null,
+              "thumb": null,
+              "vaultId": {
+                "development": null,
+                "release": null
+              }
+            }
+          ]
+        },
+      ],
+    }
+  },
   "templates": {
     "slide": {
       "id": "",
